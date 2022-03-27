77e6347edc4e647ad60b8166e714897281f100c1
thieusumo <thieu.nguyen@silkwires-dev.com>
2022-03-27 09:45:23 +0700

    init project laravel 9

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> @yield('title') | Laravel 9</title>
+
+    <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
+
+</head>
+<body>
+    
+    @yield('content')
+
+</body>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</html>
