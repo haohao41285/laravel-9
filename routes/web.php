fd3e3ba453207a0b8da29432e5555605c6b32962
haohao <nguyenthieupro93@gmail.com>
2022-10-02 23:06:57 +0700

    add blog page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('blog.html', function() {
+    return view('blog');
+});
+
+Route::get('{slug}', function($slug) {
+    return $slug;
+});
